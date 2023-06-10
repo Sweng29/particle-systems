4349e0b7e4cc36ae2181c5e8d1c40d2a11307e97
Kashif Ali <kashifali@ophycare.com>
2023-06-10 13:39:54 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = "white";
+ctx.fillRect(150, 150, 100, 200);
+
+console.log(ctx);
+
+class Particle {}
+
+class Effect {}
+
+function animate() {}
