2cb6f9329953beebc6c914375b58a57865bf66c4
Kashif Ali <kashifali@ophycare.com>
2023-06-10 14:43:23 +0400

    Constellation particales

--- main.js
+++ main.js
@@ -2,23 +2,26 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.fillStyle = "red";
 
-console.log(ctx);
+const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+gradient.addColorStop(0, "white");
+gradient.addColorStop(0.5, "magenta");
+gradient.addColorStop(1, "blue");
+ctx.fillStyle = gradient;
+ctx.strokeStyle = "white";
 
 class Particle {
   constructor(effect) {
     this.effect = effect;
-    this.radius = Math.random() * 40 + 5;
+    this.radius = Math.random() * 5 + 2;
     this.x =
       this.radius + Math.random() * (this.effect.width - this.radius * 2);
     this.y =
       this.radius + Math.random() * (this.effect.height - this.radius * 2);
-    this.vx = Math.random() * 4 - 2;
-    this.vy = Math.random() * 4 - 2;
+    this.vx = Math.random() * 1 - 0.5;
+    this.vy = Math.random() * 1 - 0.5;
   }
   draw(context) {
-    context.fillStyle = "hsl(" + this.x * 0.5 + ", 100%, 50%)";
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.fill();
@@ -39,7 +42,7 @@ class Effect {
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.particles = [];
-    this.numberOfParticles = 100;
+    this.numberOfParticles = Math.random() * 200 + 100;
     this.createParticles();
   }
 
@@ -50,10 +53,32 @@ class Effect {
   }
 
   handleParticles(context) {
+    this.connectParticles(context);
     this.particles.forEach((particle) => {
       particle.draw(context);
       particle.update();
     });
+  }
+
+  connectParticles(context) {
+    const maxDistance = 100;
+    for (let a = 0; a < this.particles.length; a++) {
+      for (let b = a; b < this.particles.length; b++) {
+        const dx = this.particles[a].x - this.particles[b].x;
+        const dy = this.particles[a].y - this.particles[b].y;
+        const distance = Math.hypot(dx, dy);
+        if (distance < maxDistance) {
+          context.save();
+          const opacity = 1 - distance / maxDistance;
+          context.globalAlpha = opacity;
+          context.beginPath();
+          context.moveTo(this.particles[a].x, this.particles[a].y);
+          context.lineTo(this.particles[b].x, this.particles[b].y);
+          context.stroke();
+          context.restore();
+        }
+      }
+    }
   }
 }
 
