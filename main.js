739b5c62ddb691ff4dbc0ff2d5e8737e0a99bec4
Kashif Ali <kashifali@ophycare.com>
2023-06-10 13:49:44 +0400

    canvase skeleton setup

--- main.js
+++ main.js
@@ -2,14 +2,48 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-ctx.fillStyle = "white";
-ctx.fillRect(150, 150, 100, 200);
+context.fillStyle = "red";
 
 console.log(ctx);
 
-class Particle {}
+class Particle {
+  constructor(effect) {
+    this.effect = effect;
+    this.x = Math.random() * this.effect.width;
+    this.y = Math.random() * this.effect.height;
+    this.radius = 15;
+  }
+  draw(context) {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+  }
+}
 
-class Effect {}
+class Effect {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.particles = [];
+    this.numberOfParticles = 20;
+    this.createParticles();
+  }
+
+  createParticles() {
+    for (let i = 0; i < this.numberOfParticles; i++) {
+      this.particles.push(new Particle(this));
+    }
+  }
+
+  handleParticles(context) {
+    this.particles.forEach((particle) => {
+      particle.draw(context);
+    });
+  }
+}
+
+const effect = new Effect(canvas);
+effect.handleParticles(ctx);
 
 function animate() {}
